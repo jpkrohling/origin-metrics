ac6abd9b78d4c55961e4f1bd99cd462fd4c425e6
Juraci Paixão Kröhling <juraci@kroehling.de>
2017-02-08 16:00:07 +0100

    Not working yet - Cassandra client encryption

--- hawkular-metrics/hawkular-metrics-wrapper.sh
+++ hawkular-metrics/hawkular-metrics-wrapper.sh
@@ -23,19 +23,54 @@ POD_NAMESPACE=${POD_NAMESPACE:-openshift-infra}
 MASTER_URL=${MASTER_URL:-https://kubernetes.default.svc:443}
 
 JBOSS_HOME=${JBOSS_HOME:-/opt/jboss/wildfly}
+HAWKULAR_METRICS_DIRECTORY=${HAWKULAR_METRICS_DIRECTORY:-/opt/hawkular}
+KEYSTORE_DIR=${KEYSTORE_DIR:-"${HAWKULAR_METRICS_DIRECTORY}"}
+
+KEYSTORE_FILE=${KEYSTORE_FILE:-"${KEYSTORE_DIR}/hawkular-metrics.keystore"}
+KEYSTORE_PASSWORD=${KEYSTORE_PASSWORD:-$(head /dev/urandom -c 512 | tr -dc A-Z-a-z-0-9 | head -c 17)}
+
+TRUSTSTORE_FILE=${TRUSTSTORE_FILE:-"${KEYSTORE_DIR}/hawkular-metrics.truststore"}
+TRUSTSTORE_PASSWORD=${TRUSTSTORE_PASSWORD:-$(head /dev/urandom -c 512 | tr -dc A-Z-a-z-0-9 | head -c 17)}
+
+SERVICE_CERT=${SERVICE_CERT:-"/secrets/tls.crt"}
+SERVICE_CERT_KEY=${SERVICE_CERT_KEY:-"/secrets/tls.key"}
+SERVICE_CA=${SERVICE_CA:-"/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"}
+
+PKCS12_FILE=${PKCS12_FILE:-"${KEYSTORE_DIR}/hawkular-metrics.pkcs12"}
+KEYTOOL_COMMAND="/usr/lib/jvm/java-1.8.0/jre/bin/keytool"
 
 # Check Read Permission
 token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 url="${MASTER_URL}/api/${KUBERNETES_API_VERSION}/namespaces/${POD_NAMESPACE}/replicationcontrollers/hawkular-metrics"
-cacrt="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 
-status_code=$(curl --cacert ${cacrt} --max-time 10 --connect-timeout 10 -L -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer ${token}" $url)
+status_code=$(curl --cacert ${SERVICE_CA} --max-time 10 --connect-timeout 10 -L -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer ${token}" $url)
 if [ "$status_code" != 200 ]; then
   echo "Error: the service account for Hawkular Metrics does not have permission to view resources in this namespace. View permissions are required for Hawkular Metrics to function properly."
   echo "Usually this can be resolved by running: oc adm policy add-role-to-user view system:serviceaccount:${POD_NAMESPACE}:hawkular -n ${POD_NAMESPACE}"
   exit 1
 else
   echo "The service account has read permissions for its project. Proceeding"
+fi
+
+echo "Creating the Hawkular Metrics PKCS12 keystore"
+openssl pkcs12 -export -in ${SERVICE_CERT} -inkey ${SERVICE_CERT_KEY} -out ${PKCS12_FILE} -name hawkular-metrics -noiter -nomaciter -password pass:${KEYSTORE_PASSWORD}
+if [ $? != 0 ]; then
+    echo "Failed to create a PKCS12 certificate file with the service-specific certificate. Aborting."
+    exit 1
+fi
+
+echo "Converting the PKCS12 keystore into a Java Keystore"
+${KEYTOOL_COMMAND} -v -importkeystore -srckeystore ${PKCS12_FILE} -srcstoretype PKCS12 -destkeystore ${KEYSTORE_FILE} -deststoretype JKS -deststorepass ${KEYSTORE_PASSWORD} -srcstorepass ${KEYSTORE_PASSWORD}
+if [ $? != 0 ]; then
+    echo "Failed to create a Java Keystore file with the service-specific certificate. Aborting."
+    exit 1
+fi
+
+echo "Importing the Service CA into the trust store"
+${KEYTOOL_COMMAND} -noprompt -import -alias services-ca -file ${SERVICE_CA} -keystore ${TRUSTSTORE_FILE} -trustcacerts -storepass ${TRUSTSTORE_PASSWORD}
+if [ $? != 0 ]; then
+    echo "Failed to import the Service CA into the trust store. Aborting."
+    exit 1
 fi
 
 # Setup additional logging if the ADDITIONAL_LOGGING variable is set
@@ -64,9 +99,17 @@ if [ "x${JGROUPS_PASSWORD}" == "x" ]; then
     JGROUPS_PASSWORD=$(head /dev/urandom -c 512 | tr -dc A-Z-a-z-0-9 | head -c 17)
 fi
 
+cat > ${HAWKULAR_METRICS_DIRECTORY}/server.properties << EOL
+EOL
+
 exec 2>&1 /opt/jboss/wildfly/bin/standalone.sh \
   -Djboss.node.name=$HOSTNAME \
   -Djgroups.password=${JGROUPS_PASSWORD} \
+  -Djavax.net.ssl.keyStore=${KEYSTORE_FILE} \
+  -Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} \
+  -Djavax.net.debug=ssl \
+  -Djavax.net.ssl.keyStorePassword=${KEYSTORE_PASSWORD} \
+  -Djavax.net.ssl.trustStorePassword=${TRUSTSTORE_PASSWORD} \
   -b `hostname -i` \
   -bprivate `hostname -i` \
   $@
